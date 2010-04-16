2629603796e5155a77dc1966e007a06ad37335f9
Vincent Bruijn <vincentbruijn2@macbook-van-vincent-bruijn.local>
2010-04-16 16:57:39 +0200

    changes after consulting ax710

--- getline.php
+++ getline.php
@@ -1,6 +1,12 @@
 <?php
 error_reporting(0);
-
+sleep(2);
+if (!isset($_GET['sp'])) {
+	header('X-Exception: send correct get params');
+	echo 'No direct access';
+	exit;
+}
+$sp = (int) $_GET['sp'];
 $url = "http://www.metropolism.com";
 $req = '/magazine/';
 
@@ -10,17 +16,26 @@ $issues = array();
 for ($i = 4; $i <= date('y'); $i++) {
 	array_push($years, 2000 + $i);
 }
+$y = $years[rand(0, count($years) - 1)];
+$m = 6; // Metropolis M issues every 2 months
 
-for ($j = 1; $j <= 6; $j++) {
+if ($y == date('Y')) { // keep up with the current issues
+	$m = floor(date('n') / 2);
+	$m = $m == 0 ? ++$m : $m;
+}
+
+for ($j = 1; $j <= $m; $j++) {
 	array_push($issues, '-no' . $j);
 }
-$req .= $years[rand(0, count($years) - 1)] . $issues[rand(0, count($issues) - 1)] . '/';
+$req .= $y . $issues[rand(0, count($issues) - 1)] . '/';
 
 $doc = new DOMDocument();
 $doc->strictErrorChecking = false;
+$baseDoc->encoding = 'UTF-8';
 @$doc->loadHTML(@file_get_contents($url . $req));
 $content = $doc->getElementById('content');
 if (get_class($content) !== 'DOMElement') {
+	header('X-Exception: Content is no DOMElement');
 	echo 'axel';
 	die;
 }
@@ -38,6 +53,7 @@ foreach ($linkStrings as $link) {
 	}
 }
 if (count($artLinks) == 0) {
+	header('X-Exception: No artlinks');
 	echo 'axel';
 	die;
 }
@@ -47,10 +63,27 @@ $baseDoc->strictErrorChecking = false;
 $baseDoc->encoding = 'UTF-8';
 @$baseDoc->loadHTML(@file_get_contents($url . $baseContentLink));
 $baseContent = $baseDoc->getElementById('articleContent');
+// echo $baseContent->nodeValue;
+// preg_match("/[A-Z]{1}[a-z ][\w\s]*\. /",$baseContent->nodeValue, $matches);
+header("Content-Type: text/html; charset=UTF-8");
+switch ($sp) {
+	case '0':
+		preg_match_all("/[A-Z]{1}[a-z]*[ ]{1}([a-z0-9:\-,]*[ ]){4,12}/",$baseContent->nodeValue, $matches);
+		break;
+	case '1':
+		preg_match_all("/[ ]{1}[a-z]{1}([A-Za-z\-,:]*[ ]){6,}/",$baseContent->nodeValue, $matches);
+		break;
+	case '2':
+		preg_match_all("/([ ]+?[a-z][A-Za-z\-,:]*){6,13}([\.\?!]){1}/",$baseContent->nodeValue, $matches,PREG_PATTERN_ORDER);
+		break;
+}
 
-preg_match("/[A-Z]{1}[a-z ][\w\s]*\. /",$baseContent->nodeValue, $matches);
-
-echo isset($matches[0]) ? $matches[0] : 'axel';
+//preg_match("/[A-Z]{1}[ ]/",$baseContent->nodeValue, $matches);
+// print_r($matches);
+// echo str_repeat('<br />', 4);
+// var_dump($baseContent->nodeValue);
+echo isset($matches[0]) && count($matches[0]) > 0 ? $matches[0][rand(0, count($matches[0]) - 1)] : 'axel';
+//echo isset($matches[0]) ? $matches[0] : 'axel';
 
 exit;
 
--- index.php
+++ index.php
@@ -1,37 +1,67 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
-	"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
+	"http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
 	<title>TJ</title>
 	<script type="text/javascript" src="js/mootools.js"></script>
-
+	<script type="text/javascript" src="js/mootools-more.js"></script>
+	<script type="text/javascript">
+		window.addEvent('load', function() {
+			$('content').addEvent('mycomplete', function() {
+				getLine();
+			});
+			$('content').fireEvent('mycomplete');
+		});
+		window.cC = 0;
+		window.sP = 0;
+	</script>
+	<style type="text/css">
+	.color0 { color: rgb( 35,  31,  32); }
+	.color1 { color: rgb(236,   0, 140); }
+	.color2 { color: rgb(  0, 174, 239); }
+	.color3 { color: rgb(255, 242,   0); }
+	</style>
 </head>
 
-<body onload="getLine.periodical(8000)">
-	<h2>TJ</h2>
-<div id="content">
+<body>
+<div id="content" style="margin-bottom: 45px;">
 	
 </div>
+<div style="position: fixed; bottom: 0; padding: 4px; background: #fff;">
+<a rel="license" href="http://creativecommons.org/licenses/by/3.0/nl/"><img alt="Creative Commons License" style="border-width:0; float: left; clear: both; margin-right: 4px;" src="http://creativecommons.org/images/public/somerights20.png" /></a>
+<span class="color2" xmlns:dc="http://purl.org/dc/elements/1.1/" property="dc:title">TJ</span> van <a class="color1" xmlns:cc="http://creativecommons.org/ns#" href="http://www.ax710.org/" property="cc:attributionName" rel="cc:attributionURL">ax710 &amp; alweervincent</a> is in licentie gegeven volgens een <a class="color2" rel="license" href="http://creativecommons.org/licenses/by/3.0/nl/">Creative Commons Naamsvermelding 3.0 Nederland licentie</a>.<br />Gebaseerd op een script van <a class="color3" xmlns:dc="http://purl.org/dc/elements/1.1/" href="http://www.vincentbruijn.nl/tj/" rel="dc:source">www.vincentbruijn.nl</a>.
+</div>
 <script type="text/javascript">
-
 function getLine() {
 	var r = new Request({
 		method:'get',
-		url:"getline.php",
+		url:"getline.php?sp=" + window.sP,
 		onSuccess: addResp
 	}).send();
 }
 
 function addResp(resp) {
-	if (resp == 'axel') {
-		return;
-	}
 	d = $('content');
-	el = new Element('div');
-	el.set('html', resp);
-	el.injectInside(d);
+	if (resp !== 'axel') {
+		el = new Element('span');
+		el.setProperty('class', 'color' + window.cC);
+		el.set('html', resp + ' ');
+		el.injectInside(d);
+		if (window.sP == 2) {
+			window.sP = 0;
+			if (window.cC == 3) {
+				window.cC = 0;
+			} else {
+				window.cC++;
+			}
+		} else {
+			window.sP++;
+		}
+		var myFx = new Fx.Scroll(window).toBottom();
+	}
+	d.fireEvent('mycomplete');
 }
 </script>
 </body>
