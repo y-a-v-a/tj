6cb73c67ed1412378f83ac101d3d07b9177fd370
Vincent Bruijn <vebruijn@gmail.com>
2014-11-05 22:27:49 +0100

    nicer generation of 50k word novel #NaNoGenMo-2014

--- bin/generate.php
+++ bin/generate.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(0);
 require '../www/BibleLine.class.php';
 
 $sp = 0;
@@ -6,13 +7,26 @@ $b = new BibleLine($sp);
 
 $text = '';
 $now = date("Ymd-His");
+$fileName = './text' . $now . '.txt';
 
 while (str_word_count($text) < 50000) {
     $line = $b->getLine();
-    echo $line;
+    // echo $line;
     $text .= $line;
-    file_put_contents('./text' . $now . '.txt', $line, FILE_APPEND);
-    $b->changeSp(++$sp % 3);    
+    file_put_contents($fileName, $line, FILE_APPEND);
+    $b->changeSp(++$sp % 3);
+}
+
+// create a nice ending
+ while (true) {
+    $line = $b->getLine();
+    if (preg_match('/\n\n/', $line)) {
+        $line = strrev(strstr(strrev($line), "\n\n"));
+        @file_put_contents($fileName, $line, FILE_APPEND);
+        exit;
+    }
+    @file_put_contents($fileName, $line, FILE_APPEND);
+    $b->changeSp(++$sp % 3);
 }
 
 exit;
--- www/BibleLine.class.php
+++ www/BibleLine.class.php
@@ -15,7 +15,20 @@ class BibleLine {
     
     private $cache = array();
     
+    private $last = 2;
+    
+    private $sentenceCount = 0;
+    
+    private $sentenceMax = 0;
+    
+    private $paragraphCount = 0;
+    
+    private $titleAfterNParagraphs = 0;
+    
     public function __construct($sp) {
+        if ($this->sentenceMax === 0) {
+            $this->sentenceMax = rand(3, 12);
+        }
         $this->sp = $sp;
     }
     
@@ -38,12 +51,18 @@ class BibleLine {
         // strip meta-data from end of file
         $cnt = strrev(strstr(strrev($cnt), strrev('*** END')));
 
-        $cnt = substr($cnt, 0, strlen($cnt) - strlen('*** END'));
+        $cnt = trim(str_replace('*** END', '', $cnt), " \n\t\r");
+        // $cnt = substr($cnt, 0, strlen($cnt) - strlen('*** END'));
+
+        if (!($this->sp == $this->last + 1 || $this->sp == $this->last - 2)) {
+            $this->sp = $this->last + 1 == 3 ? 0 : $this->last + 1;
+        }
 
         // do some regexp to get the right sentence part
         switch ($this->sp) {
         	case '0':
         		preg_match_all("/[A-Z]{1}[a-z]*[ ]{1}([a-z0-9:\-,]*[ ]){4,12}/",$cnt, $matches);
+                $matches[0] = array_map(function($item) { return trim($item, "\n\r\t;:. ,"); }, $matches[0]);
         		break;
         	case '1':
         		preg_match_all("/[ ]{1}[a-z]{1}([A-Za-z\-,:]*[ ]){6,}/",$cnt, $matches);
@@ -56,29 +75,51 @@ class BibleLine {
         if (isset($matches[0]) && count($matches[0]) > 0) {
             $index = rand(0, count($matches[0]) - 1);
             $line = trim($matches[0][$index]);
+            $this->last = $this->sp;
+
             if ($this->sp === 0) {
                 $line = ucfirst($line);
             } else if ($this->sp === 2 && !preg_match("/[\.\?\!]/", substr($line, -1))) {
                 $line .= '.';
             }
-        	return $line . ' ';
+            $line .= ' ';
+            if ($this->sp === 2) {
+                $this->sentenceCount++;
+                if ($this->sentenceCount === $this->sentenceMax) {
+                    $lineBreakCount = rand(0, 3) === 0 ? 2 : 1;
+                    $line .= str_repeat("\n", $lineBreakCount);
+                    $this->sentenceCount = 0;
+                    $this->sentenceMax = rand(4, 12);
+                    $this->paragraphCount++;
+                    if ($lineBreakCount === 2 && $this->paragraphCount >= $this->titleAfterNParagraphs) {
+                        $newIndex = rand(0, count($matches[0]) - 1);
+                        $parts = explode(" ", trim(trim($matches[0][$index]), ",.?!:"));
+                        $title = implode(" ", array_slice($parts, 0, rand(3, min(6, count($parts)))));
+                        $line .= ucfirst($title) . "\n";
+                        $this->titleAfterNParagraphs = rand(5, 10);
+                    }
+                }
+            }
+            // echo '.';
+        	return $line;
         }
         return '';
     }
     
     private function getUrl() {
         $baseUrl = $this->urls[rand(0, count($this->urls) - 1)];
-        $book = $this->getBook();
-        return $baseUrl . '/' . $book;
+        return $baseUrl . '/';
     }
     
     private function getBook() {
-        $book = str_pad((string) rand(0,73), 2, '0', STR_PAD_LEFT);
+        $book = str_pad((string) rand(1,73), 2, '0', STR_PAD_LEFT);
         return 'drb' . $book . '10.txt';
     }
     
     private function getRawData($url) {
-        $hash = md5($url);
+        $book = $this->getBook();
+        $url = $url . $book;
+        $hash = md5($book);
         if (array_key_exists($hash, $this->cache)) {
             return $this->cache[$hash];
         }
@@ -86,6 +127,7 @@ class BibleLine {
         if ($raw !== false) {
             $this->cache[$hash] = $raw;
         }
+        // print(count($this->cache));
         return $raw;
     }
     
